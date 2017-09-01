22650ca68594405c25de7db5b4209e0209854c09
Michael Wolber <mwolber@gmx.de>
2017-09-02 00:10:51 +0200

    Added generated comment to the default template.

--- embed.go
+++ embed.go
@@ -6,15 +6,19 @@
 
 package main
 
+// embed is a generated function returning the template as a string.
+// That string should be parsed by the functions of the template package.
 func embedTemplate() string {
 	var tmpl = "/*\n" +
-		" * CODE GENERATED AUTOMATICALLY WITH \n" +
-		" *    github.com/wlbr/templify \n" +
+		" * CODE GENERATED AUTOMATICALLY WITH\n" +
+		" *    github.com/wlbr/templify\n" +
 		" * THIS FILE SHOULD NOT BE EDITED BY HAND\n" +
 		" */\n" +
 		"\n" +
 		"package {{.Package}}\n" +
 		"\n" +
+		"// {{.Name}} is a generated function returning the template as a string.\n" +
+		"// That string should be parsed by the functions of the template package.\n" +
 		"func {{.Name}}Template() string {\n" +
 		"\tvar tmpl = \"{{.Content}}\"\n" +
 		"\treturn tmpl\n" +
--- embed.tpl
+++ embed.tpl
@@ -1,11 +1,13 @@
 /*
- * CODE GENERATED AUTOMATICALLY WITH 
- *    github.com/wlbr/templify 
+ * CODE GENERATED AUTOMATICALLY WITH
+ *    github.com/wlbr/templify
  * THIS FILE SHOULD NOT BE EDITED BY HAND
  */
 
 package {{.Package}}
 
+// {{.Name}} is a generated function returning the template as a string.
+// That string should be parsed by the functions of the template package.
 func {{.Name}}Template() string {
 	var tmpl = "{{.Content}}"
 	return tmpl
